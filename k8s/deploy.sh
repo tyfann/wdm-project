efeff4fcf5d4f3f7b4362460a1f5247cbca2897a
tyfann <1342344773@qq.com>
2023-06-07 15:37:53 +0200

    script changed.

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,4 +1,5 @@
-minikube start --memory 3000
+minikube start --memory 8000 --cpus 8
+minikube addons enable metrics-server
 minikube addons enable ingress
 kubectl delete -A ValidatingwebhookConfiguration ingress-nginx-admission
 kubectl create \
@@ -13,4 +14,11 @@ kubectl create -f ingress-service.yaml
 # One time database initialization
 kubectl create -f cluster-init.yaml
 kubectl create -f db-init.yaml
+
+kubectl -n kube-system rollout status deployment metrics-server
+kubectl autoscale deployment stock-deployment --cpu-percent=50 --min=1 --max=5
+kubectl autoscale deployment order-deployment --cpu-percent=50 --min=1 --max=5
+kubectl autoscale deployment payment-deployment --cpu-percent=50 --min=1 --max=5
+kubectl autoscale deployment connector-deployment --cpu-percent=50 --min=1 --max=3
+
 minikube tunnel
